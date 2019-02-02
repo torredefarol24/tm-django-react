89dfd3f69f7c5ebd310728fe80373dc9d2c6a1ad
Toufiq <burningraven06@gmail.com>
2019-02-02 18:39:48 +0600

    include header Comp in App Comp

--- leadmanager/reactfrontend/src/components/App.js
+++ leadmanager/reactfrontend/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './layouts/header'
 
 class App extends React.Component{
 	render(){
 		return (
 			<div className='container'>
-				<h1> Hey From React</h1>
+				<Header></Header>
 			</div>
 		)
 	}
